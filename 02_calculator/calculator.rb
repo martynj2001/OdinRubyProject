879b069bd3d2e69c7cfc31fd16964b6d9083330f
Martyn <martynj2010@icloud.com>
2017-03-24 22:35:47 +0000

    02_calculator complete, addition task 1 complete

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(num1, num2)
+	num1 + num2
+end
+	
+def subtract(num1, num2)
+	num1 - num2
+end
+
+def sum(array)
+	total = 0
+	array.each do |num|
+		total += num
+	end
+	total	
+end
+
+def multiply (num_array)
+	total = 1
+	num_array.each do |num|
+		total *= num
+	end
+	total
+end 
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -77,11 +77,16 @@ end
 # once the above tests pass,
 # write tests and code for the following:
 
-describe "#multiply" do
+describe "multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect(multiply([1,2])).to eq(2)
+  end
 
-  it "multiplies several numbers"
+  it "multiplies several numbers" do
+    expect(multiply([1,2,3])).to eq(6)
+  end
+
   
 end
 
