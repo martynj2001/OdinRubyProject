17d3f6ff5494cc5160f9d61548f9695ee250bafa
Martyn <martynj2010@icloud.com>
2017-03-28 18:20:23 +0100

    03_Simon_Says -- 50% complete

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,18 @@
 #write your code here
+def echo text
+	puts text
+	text
+end
+
+def shout text
+	puts text.upcase
+	text.upcase
+end
+
+def repeat(text, times=1)
+	repeat = 1
+	while repeat <= times
+	 text = text + " " + text
+	end
+end
+
