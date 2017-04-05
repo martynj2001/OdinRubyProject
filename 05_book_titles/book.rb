0ef35d0e49eb0a832db4873bc7d4a0011cc1cebf
Martyn <martynj2010@icloud.com>
2017-04-05 20:26:45 +0100

    05_book_titles Complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
 # write your code here
+	attr_accessor :title
+
+	def title
+
+		exceptions = ["a","the","an","of","and","in"]
+		title_cap = []
+		title_array = @title.split(" ")
+
+		title_array.each do |word|
+			if not exceptions.include? word
+				word = word.capitalize
+				title_cap.push(word)
+			else
+				title_cap.push(word)
+			end
+		end
+		title_cap[0] = title_cap[0].capitalize
+		@title = title_cap.join(" ")		
+	end
+
 end
